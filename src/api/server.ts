ce2683dd6bd70d90d5ca5212469273bc2ceb3059
Nerots <daniuyan71@gmail.com>
2024-01-02 18:45:40 +0700

    adding cors

--- src/api/server.ts
+++ src/api/server.ts
@@ -3,6 +3,7 @@ import express, { Application } from "express";
 import Database from "./database/db.js";
 import routesUser from "./routes/routes.user.js";
 import routesProduct from "./routes/routes.product.js";
+import cors from "cors";
 
 class App {
   public app: Application = express();
@@ -21,6 +22,13 @@ class App {
 
   private server(): void {
     this.app.use(express.json());
+
+    this.app.use(
+      cors({
+        methods: ["GET", "POST", "PATCH", "DELETE"],
+        origin: process.env.CLIENT_URL,
+      })
+    );
 
     this.app.use(express.static("public"));
   }
